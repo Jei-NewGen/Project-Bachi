ba34d80f36b1ff05a57ce8bd6a9bc37b7966f93c
Jei-NewGen <zenonfan120@gmail.com>
2026-08-10 20:30:00 -0500

    added some more changes to the link section and customized the pop up to dispay random images, i usually only learn code while doing the curriculum on freecodecamp but when i want a feature and look around i find code and break it down to where i understand what every line is doing and learning stuff before i do it within the curriculum don't know if thats bad or not but im happy to keep learning and coding every day

--- hishaku-hq.js
+++ hishaku-hq.js
@@ -1,5 +1,10 @@
 const bachiQuotes=[
-"Katanas have always saved lives and slashed open eras. But...I'm sure you know...They're still tools for killing people. We are complicit in the deaths they cause. - Kunishige Rokuhira", "I'll cut them down, I'll cut them all down! - Chihiro Rokuhira",
+"Katanas have always saved lives and slashed open eras. But...I'm sure you know...They're still tools for killing people. We are complicit in the deaths they cause. - Kunishige Rokuhira", 
+"I'll cut them down, I'll cut them all down! -Chihiro Rokuhira",
+"I'll face him head on... and give him an honorable death. -Chihiro Rokuhira",
+"I just happened to have someone... who set me on the right path. -Hakuri Sazanami", 
+"Its my fault for being so useless. I can't do anything. That's why i want to be useful...even a little bit. -Hakuri Sazanami",
+
 ];
 const randomSelect = Math.floor(Math.random() * bachiQuotes.length );
 
@@ -10,3 +15,10 @@ const helloThere = document.getElementById("quote-random");
 helloThere.innerText = bachiSelect;
 
 helloThere.style.setProperty("display","block","important");
+
+const quoteBackgroundImages=[
+    "url('assets/chihiro png.png')", "url('assets/chihiro 2.png')", "url('assets/chihiro 3.png')", "url('assets/chihiro 4.png')"
+];
+
+const randomGrab = Math.floor(Math.random() * quoteBackgroundImages.length);
+document.getElementById("quote-random").style.backgroundImage = quoteBackgroundImages[randomGrab];
