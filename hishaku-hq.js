7956d747fd716e662448a4606c8c9419046411a1
Jei-NewGen <zenonfan120@gmail.com>
2026-08-11 10:30:01 -0500

    made two sections under for the author and learned some JS that allows me to make a different quote from the author appear every few seconds. I love how i have barely touched the surface of the cool things i can do and apply. Excited for the art and gacha game section those will require quite the creativity to look good, also realized i have to design for not just desktop but ipad too but ill make its design similar or identical to the mobile layout. Desktop will have its own design i feel i can do and apply more since its bigger anyway.

--- hishaku-hq.js
+++ hishaku-hq.js
@@ -22,3 +22,26 @@ const quoteBackgroundImages=[
 
 const randomGrab = Math.floor(Math.random() * quoteBackgroundImages.length);
 document.getElementById("quote-random").style.backgroundImage = quoteBackgroundImages[randomGrab];
+
+const authorCommentsHokazono=[
+    "I did the thing where you go into a bookstore and buy your own book. It made me feel so happy.",
+    "I like to think up my storyboards while I take a walk, but it's now so cold that I can't leave the house. What am I supposed to do?!",
+    "When I walk around at night while listening to the soundtrack of The Batman, I become Batman.",
+    "When I hear “Bling-Bang-Bang-Born,” I need to wave my arms, so I have to suddenly stop working. I can't find a solution to this.",
+    "I was one of those terrible kids who would ask his parents for cash as a birthday present. But what could I do, I was addicted to DB Heroes.",
+    "I’ve been watching Pixar movies during work lately. They’re great and bring back so many memories. The opening to Cars is the best!",
+    "Getting Todo into my life has definitely made it much better. My weekly pleasures are disappearing one by one…",
+    "Right now my hair is the longest it’s ever been. It makes my neck itchy, so I want to cut it. Also, Tom Hardy is really cool lately.",
+    "I watched Thunderbolts. My first Marvel movie in theaters in a while. Florence Pugh is one of a kind.",
+    "I went to NY! Thanks to all the support of the editorial department, I was able to have a blast even with my terrible English!",
+    "Don’t end, Modulo!!!!!!!!!!",
+    "I used Golden Week to go see a Kagurabachi anime announcement billboard, but it was already down. I went to look at a white wall!",
+    
+]
+const messageReplace = document.getElementById("replaceText");
+function commentUpdate(){
+const randomComment = Math.floor(Math.random()* authorCommentsHokazono.length);
+messageReplace.innerText = authorCommentsHokazono[randomComment];
+};
+commentUpdate();
+setInterval(commentUpdate,7000);
