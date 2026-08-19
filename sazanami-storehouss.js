f0f17334cf1dbb5aff9163f989ba608d303566f7
Jei-NewGen <zenonfan120@gmail.com>
2026-08-19 04:28:50 -0500

    worked on the formation and layout of the volume covers section and its coming out looking nicer than i had predicted, just need to finish the JS section for the random cover showing at intervals of a few seconds, kinda went off the lesson curriculum and learned this and how it worked myself so when something goes wrong i have to go back and look over or i sit there and scan the code line by line to see where i went wrong

--- sazanami-storehouss.js
+++ sazanami-storehouss.js
@@ -1,4 +1,7 @@
-const volumes = [
+const volumes = ["url('assets/kgb volume 1 cover.webp')","url('assets/kgb volume 2.webp')","url('assets/kgb vol 3.webp')",
+    "url('assets/kgb vol 4.webp')","url('assets/kgb vol 5.webp')","url('assets/kgb vol 6.webp')",
+    "url('assets/kgb vol 7.webp')","url('assets/kgb vol 8.webp')","url('assets/')",
+    "url('assets/')","url('assets/')"
     
 ]
 
