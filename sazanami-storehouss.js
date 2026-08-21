eada05cce08be798878fd439ceb0a76e7e1da180
Jei-NewGen <zenonfan120@gmail.com>
2026-08-21 18:31:51 -0500

    attempting to add a feature where the volumes randomly shift between the images but code isnt working so debugging it, also made a few small changes around the website pages

--- sazanami-storehouss.js
+++ sazanami-storehouss.js
@@ -1,16 +1,20 @@
-const volumes = ["url('assets/kgb volume 1 cover.webp')","url('assets/kgb volume 2.webp')","url('assets/kgb vol 3.webp')",
-    "url('assets/kgb vol 4.webp')","url('assets/kgb vol 5.webp')","url('assets/kgb vol 6.webp')",
-    "url('assets/kgb vol 7.webp')","url('assets/kgb vol 8.webp')","url('assets/')",
-    "url('assets/')","url('assets/')"
-    
+const volumes = ["url('assets/kgb volume 1 cover.webp')",
+  "url('assets/kgb volume 2.webp')",
+  "url('assets/kgb vol 3.webp')",
+  "url('assets/kgb vol 4.webp')",
+  "url('assets/kgb vol 5.webp')",
+  "url('assets/kgb vol 6.webp')",
+  "url('assets/kgb vol 7.webp')",
+  "url('assets/kgb vol 8.webp')",
+  "url('assets/kgb vol 9.webp')",
+  "url('assets/kgb vol 10.webp')",
+  "url('assets/kgb vol 11.webp')"
 ]
-
-
 
 const volumeChange = document.getElementById("Change-1");
 function volumeReplace(){
 const randomVolume = Math.floor(Math.random() * volumes.length);
-volumeChange.innertext = volumes[randomVolume];
+volumeChange.src = volumes[randomVolume];
 }
 volumeReplace();
-setInterval(volumeReplace, 6000);
+setInterval(volumeReplace, 7000);
