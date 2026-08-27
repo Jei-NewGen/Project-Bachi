5f62559af1f85a2476dae180a60d976ad3bb0e46
Jei-NewGen <zenonfan120@gmail.com>
2026-08-26 23:42:16 -0500

    made a good amount of progress and i got my random generator to work first try, nice! Just have to add some finishing touches on the layout and how it looks and then this section will be complete for the webpage, still a long way to go tho and have to do the desktop and tablet designs and adjustments!

--- sazanami-storehouse.js
+++ sazanami-storehouse.js
@@ -22,8 +22,26 @@ setInterval(volumeReplace, 7000);
 
 
 const memeChange = document.getElementById("Change-2");
-const memes = [
-
+const memes = ["url('assets/gem bachi meme 1.webp')",
+  "url('assets/gem meme 2.webp')",
+  "url('assets/gem meme 3.webp')",
+  "url('assets/oh the memes ive seen them all bachi.jpg')",
+  "url('assets/peak bachi meme.webp')",
+  "url('assets/funny bachi meme.jpg')",
+  "url('assets/iconic bachi meme.webp')",
+  "url('assets/classic bachi meme.webp')",
+  "url('assets/gojo diffed by bachi.webp')",
+  "url('assets/the honored bachi.jpg')",
+  "url('assets/lets lock in bachi man.webp')",
+  "url('assets/kgb meme 12.jpg')",
+  "url('assets/kgb meme 13.jpg')",
+  "url('assets/kgb meme 14.jpg')",
+  "url('assets/kgb meme 15.jpg')",
+  "url('assets/kgb meme 16.jpg')",
+  "url('assets/kgb meme 17.jpg')",
+  "url('assets/kgb meme 18.jpg')",
+  "url('assets/kgb meme 19.jpg')",
+  "url('assets/kgb meme 20.jpg')"
 ];
 function memeReplace(){
   const randomMeme = Math.floor(Math.random()* memes.length);
